2c760e8b8a51f1c72533c56bb6aacbd1916966ad
Diogo Capdeville <diogoj.r.c@hotmail.com>
2025-02-21 22:40:15 -0300

    feat: add product details page

---
+++ src/app/[slug]/menu/[productId]/components/Details.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { Prisma } from "@prisma/client";
+import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/helpers/formatCurrency";
+
+interface ProductDetailsProps {
+  product: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          name: true;
+          avatarImageUrl: true;
+        };
+      };
+    };
+  }>;
+}
+
+const ProductDetails = ({ product }: ProductDetailsProps) => {
+  const [quantity, setQuantity] = useState<number>(1);
+
+  const handleDecreaseQuantity = () => {
+    setQuantity((prev) => {
+      if (prev === 1) {
+        return 1;
+      }
+
+      return prev - 1;
+    });
+  };
+
+  const handleIncreaseQuantity = () => {
+    setQuantity((prev) => {
+      if (prev >= 99) {
+        return 99;
+      }
+
+      return prev + 1;
+    });
+  };
+
+  return (
+    <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col rounded-t-3xl bg-white p-5">
+      <div className="flex-auto">
+        <div className="flex items-center gap-1.5">
+          <Image
+            src={product.restaurant.avatarImageUrl}
+            alt={product.restaurant.name}
+            width={16}
+            height={16}
+            className="rounded-full"
+          />
+          <p className="text-xs text-slate-400">{product.restaurant.name}</p>
+        </div>
+
+        <h2 className="mt-1 font-semibold">{product.name}</h2>
+
+        <div className="mt-3 flex items-center justify-between">
+          <h3 className="text-xl font-semibold">
+            {formatCurrency(product.price)}
+          </h3>
+
+          <div className="flex items-center gap-2 text-center">
+            <Button
+              variant="outline"
+              className="h-8 w-8 rounded-xl transition-all duration-200 ease-linear"
+              onClick={handleDecreaseQuantity}
+              disabled={quantity === 1}
+            >
+              <ChevronLeftIcon />
+            </Button>
+
+            <p className="w-5">{quantity}</p>
+
+            <Button
+              variant="destructive"
+              className="h-8 w-8 rounded-xl transition-all duration-200 ease-linear"
+              onClick={handleIncreaseQuantity}
+              disabled={quantity >= 99}
+            >
+              <ChevronRightIcon />
+            </Button>
+          </div>
+        </div>
+
+        <div className="mt-6 space-y-2">
+          <h4 className="font-semibold">Sobre</h4>
+          <p className="text-sm text-muted-foreground">{product.description}</p>
+        </div>
+
+        <div className="mt-6 space-y-2">
+          <div className="5 flex items-center gap-1.5">
+            <ChefHatIcon size={18} />
+            <h4 className="font-semibold">Ingredientes</h4>
+          </div>
+          <ul className="text-muted-fo list-disc px-5 text-sm text-muted-foreground marker:text-xs">
+            {product.ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <Button className="mt-6 w-full rounded-full" size={"lg"}>
+        Adicionar à sacola
+      </Button>
+    </div>
+  );
+};
+
+export default ProductDetails;
--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -2,6 +2,7 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import ProductDetails from "./components/Details";
 import ProductHeader from "./components/Header";
 
 interface ProductPageProps {
@@ -11,17 +12,31 @@ interface ProductPageProps {
 const ProductPage = async ({ params }: ProductPageProps) => {
   const { slug, productId } = await params;
 
-  const product = await db.product.findUnique({ where: { id: productId } });
+  const product = await db.product.findUnique({
+    where: { id: productId },
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+          avatarImageUrl: true,
+          slug: true,
+        },
+      },
+    },
+  });
 
-  if (!product) {
+  if (
+    !product ||
+    product.restaurant.slug.toUpperCase() !== slug.toUpperCase()
+  ) {
     return notFound();
   }
 
   return (
-    <>
+    <div className="flex h-full flex-col">
       <ProductHeader product={product} />
-      <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl bg-white p-5"></div>
-    </>
+      <ProductDetails product={product} />
+    </div>
   );
 };
 
